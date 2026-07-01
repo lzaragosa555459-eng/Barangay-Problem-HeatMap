bf24b2be1d57ec84c5b55f4bdd644347ad477032
lzaragosa555459-eng <l.zaragosa.555459@umindanao.edu.ph>
2026-07-01 20:24:00 +0800

    update

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-
+// 'sudo mysql' to open mysql in wsl
+// npm run dev
+// code .
 function App() {
+
+    function text() {
+           setMessage('hii');
+    }
+
+    const [message, setMessage] = useState();
+
     return (
         <div>
             <h1>Barangay Heat Project 🚀</h1>
+            <h2>Ohayo sekay, Good morning Worlddd!!</h2>
+            <h4>{message}</h4>
+            <button onClick={text} type="button">Say hi</button>
         </div>
+        
     );
 }
 
